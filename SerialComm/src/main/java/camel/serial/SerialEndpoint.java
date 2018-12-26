3d0f57510f89b98e9634703783e0a836b0ffea56
Stephan <stephanmahieu@yahoo.com>
2018-12-26 15:56:44 +0100

    Auto discover comport

--- SerialComm/src/main/java/camel/serial/SerialEndpoint.java
+++ SerialComm/src/main/java/camel/serial/SerialEndpoint.java
@@ -2,13 +2,18 @@ package camel.serial;
 
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 
 import org.apache.camel.Component;
 import org.apache.camel.Consumer;
@@ -22,47 +27,47 @@ import org.slf4j.LoggerFactory;
 
 public class SerialEndpoint extends DefaultEndpoint {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SerialEndpoint.class);
+	private static final Logger LOG = LoggerFactory.getLogger(SerialEndpoint.class);
 
-    private SerialConfiguration config;
+	private SerialConfiguration config;
 
-    private SerialPort serialPort;
+	private SerialPort serialPort;
 	private InputStream in;
 	private OutputStream out;
-    
-    
-    public SerialEndpoint(String endPointUri, Component component, SerialConfiguration config) {
-        super(endPointUri, component);
-        this.config = config;
-        try {
+
+
+	public SerialEndpoint(String endPointUri, Component component, SerialConfiguration config) {
+		super(endPointUri, component);
+		this.config = config;
+		try {
 			serialConnect();
 		} catch (Exception e) {
 			//throw new SerialException("Error connecting to serial port " + config.getPortName(), e);
 			LOG.error("Error connecting to serial port " + config.getPortName(), e);
 		}
-    }
-    
-    @Override
-    public Consumer createConsumer(Processor processor) {
-        return new SerialConsumer(this, processor, serialPort, in);
-    }
-    
-    @Override
-    public Producer createProducer() {
-        return new SerialProducer(this, out);
-    }
+	}
 
-    @Override
-    public boolean isSingleton() {
-    	// TODO check if SerialEndpoint is singleton yes/no
-        return true;
-    }
+	@Override
+	public Consumer createConsumer(Processor processor) {
+		return new SerialConsumer(this, processor, serialPort, in);
+	}
 
-    @Override
+	@Override
+	public Producer createProducer() {
+		return new SerialProducer(this, out);
+	}
+
+	@Override
+	public boolean isSingleton() {
+		// TODO check if SerialEndpoint is singleton yes/no
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return String.format("SerialEndpoint[%s]", URISupport.sanitizeUri(getEndpointUri()));
 	}
-  
+
 //	@Override
 //	public void stop() throws Exception {
 //    	// in/out closed by producer/consumer
@@ -82,47 +87,87 @@ public class SerialEndpoint extends DefaultEndpoint {
 
 	private void serialConnect() throws Exception {
 
-    	if (LOG.isDebugEnabled()) {
-    		showAvailablePorts();
-    	}
-    	
-    	LOG.info("Connecting to port {}...", config.getPortName());
-        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(config.getPortName());
-        
-		if (portIdentifier.isCurrentlyOwned()) {
-			LOG.error("Serial port {} is currently in use, unable to connect", config.getPortName());
+		if (LOG.isDebugEnabled()) {
+			showAvailablePorts();
+		}
+
+		List<String> serialPorts = new ArrayList<>();
+		if (config.getPortName().equalsIgnoreCase("auto")) {
+			LOG.info("Attempting to connect to automatically discovered port");
+			serialPorts.addAll(getAvailablePorts());
 		}
 		else {
-			CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
+			serialPorts.add(config.getPortName());
+		}
 
-			if (commPort instanceof SerialPort) {
-				serialPort = (SerialPort) commPort;
-				
-				LOG.info("Setting serial port parameters: baudrate:{} databits:{} stopbits:{} parity:{}",
-						new Object[] { config.getBaudrate(), config.getDatabits(), config.getStopbits(), config.getParity() });
-				
-				serialPort.setSerialPortParams(
-						config.getBaudrate(), config.getDatabits(), config.getStopbits(), config.getParity());
+		serialPort = null;
 
-				in = new DataInputStream(serialPort.getInputStream());
-				out = new DataOutputStream(serialPort.getOutputStream());
+		for (String portName: serialPorts) {
 
+			LOG.info("Attempting to connect to port {}...", portName);
+
+			try {
+				CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+
+				if (portIdentifier.isCurrentlyOwned()) {
+					throw new PortInUseException();
+				}
+				else {
+					CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
+
+					if (commPort instanceof SerialPort) {
+						serialPort = (SerialPort) commPort;
+
+						LOG.info("Setting serial port parameters: baudrate:{} databits:{} stopbits:{} parity:{}",
+								new Object[]{config.getBaudrate(), config.getDatabits(), config.getStopbits(), config.getParity()});
+
+						serialPort.setSerialPortParams(
+								config.getBaudrate(), config.getDatabits(), config.getStopbits(), config.getParity());
+
+						in = new DataInputStream(serialPort.getInputStream());
+						out = new DataOutputStream(serialPort.getOutputStream());
+
+					} else {
+						throw new IllegalArgumentException("Port " + config.getPortName() + " is not a serial port!");
+					}
+				}
 			}
-			else {
-				throw new IllegalArgumentException("Port " + config.getPortName() + " is not a serial port!");
+			catch(NoSuchPortException e) {
+				LOG.error("Serial port {} does not exist, unable to connect", portName);
+			}
+			catch(PortInUseException pe) {
+				LOG.error("Serial port {} is currently in use, unable to connect", portName);
+			}
+			catch (IllegalArgumentException ie) {
+				LOG.error("Port {} is not a serial port!", portName);
+			}
+
+			if (serialPort != null) {
+				LOG.info("Connection to port {} succeeded", portName);
+				break;
 			}
 		}
-		
+
 		// TODO: close serialPort en streams
-    }
-    
-    private void showAvailablePorts() {
-    	LOG.debug("List of available commPorts...");
-    	for (@SuppressWarnings("unchecked")Enumeration<CommPortIdentifier> e = CommPortIdentifier.getPortIdentifiers(); e.hasMoreElements();) {
+	}
+
+	private void showAvailablePorts() {
+		LOG.debug("List of available commPorts...");
+		for (@SuppressWarnings("unchecked") Enumeration<CommPortIdentifier> e = CommPortIdentifier.getPortIdentifiers(); e.hasMoreElements(); ) {
 			CommPortIdentifier id = e.nextElement();
 			LOG.debug("  serial port: name:" + id.getName() + " type:" + id.getPortType() + " owner:" + id.getCurrentOwner());
 		}
-    	LOG.debug("Listing available commPorts complete.");
-    }
-    
+		LOG.debug("Listing available commPorts complete.");
+	}
+
+	private List<String> getAvailablePorts() {
+		List<String> availablePorts = new ArrayList<>();
+		for (@SuppressWarnings("unchecked") Enumeration<CommPortIdentifier> e = CommPortIdentifier.getPortIdentifiers(); e.hasMoreElements(); ) {
+			CommPortIdentifier id = e.nextElement();
+			availablePorts.add(id.getName());
+		}
+		Collections.reverse(availablePorts);
+		return availablePorts;
+	}
+
 }
