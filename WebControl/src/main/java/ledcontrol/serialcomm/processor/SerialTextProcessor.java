813732435ccdf30513d830e04903ae23306cd7b2
Stephan <stephanmahieu@yahoo.com>
2018-12-27 12:18:52 +0100

    remove websocket code (handled in camel route)

--- WebControl/src/main/java/ledcontrol/serialcomm/processor/SerialTextProcessor.java
+++ WebControl/src/main/java/ledcontrol/serialcomm/processor/SerialTextProcessor.java
@@ -1,26 +1,17 @@
 package ledcontrol.serialcomm.processor;
 
 import camel.serial.model.SerialMessage;
-//import ledcontrol.websocket.WebSocketSender;
 import org.apache.camel.Exchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * Created by Stephan on 22-12-2015.
- */
 public class SerialTextProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(SerialTextProcessor.class);
-
-//    @Autowired
-//    WebSocketSender webSocketSender;
 
     public void receiveSerialData(Exchange exchange) {
         SerialMessage serialMessage = exchange.getIn(SerialMessage.class);
         String serialData = (String)serialMessage.getBody();
 
-//        webSocketSender.send(serialData);
         LOG.info("TEXT Data received: [{}]", serialData);
     }
 }
