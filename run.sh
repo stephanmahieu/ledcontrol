c6c011e570a9560c68bfb5b60cd2c51ce11dbccb
Stephan <stephanmahieu@yahoo.com>
2018-12-27 12:06:15 +0100

    Add websocket monitor, switch to Jetty, cleanup pom

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# export MAVEN_OPTS="-Djava.library.path=/usr/lib/jni/ -Dproperties.dir=classpath:"
+# export MAVEN_OPTS="-Djava.library.path=/usr/lib/jni/"
 # mvn spring-boot:run
 
-java -Djava.library.path=/usr/lib/jni/ -Dproperties.dir=classpath: -jar WebControl/target/WebControl.jar
+java -Djava.library.path=/usr/lib/jni/ -jar WebControl.jar
