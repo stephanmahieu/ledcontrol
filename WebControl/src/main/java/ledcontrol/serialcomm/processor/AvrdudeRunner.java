c6b8b1108f7d8ef1bd39260e81f1223f3bb6223a
Stephan <stephanmahieu@yahoo.com>
2019-01-08 21:06:50 +0100

    Refactor exception handling, use ExecutorService

--- WebControl/src/main/java/ledcontrol/serialcomm/processor/AvrdudeRunner.java
+++ WebControl/src/main/java/ledcontrol/serialcomm/processor/AvrdudeRunner.java
@@ -10,10 +10,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class AvrdudeRunner {
     private static final Logger LOG = LoggerFactory.getLogger(AvrdudeRunner.class);
+
+    private static final long MAX_TIME_FOR_PROGRAM_TO_FINISH = 30L;
 
     @EndpointInject(uri = "direct:sendWebsocketData")
     private ProducerTemplate websocketLog;
@@ -30,42 +34,39 @@ public class AvrdudeRunner {
 
         ProcessBuilder pb = new ProcessBuilder();
         if (isWindows) {
-            pb.command("cmd.exe", "/c", "dir");
+            pb.command("cmd.exe", "/c", "dir /ogen");
         } else {
-            pb.command("sh", "-c", "ls");
+            pb.command("sh", "-c", "ls -ltr");
         }
         pb.directory(new File(System.getProperty("user.home")));
 
         try {
             websocketLog.sendBody("Running program...");
             LOG.info("Running program...");
+
             Process process = pb.start();
 
-            captureOutput(process.getInputStream(), OutType.STDOUT);
-            captureOutput(process.getErrorStream(), OutType.STDERR);
+            ExecutorService executor = Executors.newFixedThreadPool(2);
+            executor.execute( createOutputThread(process.getInputStream(), OutType.STDOUT));
+            executor.execute( createOutputThread(process.getErrorStream(), OutType.STDERR));
 
-            // Wait for the exit value
-            try {
-                if (process.waitFor(30L, TimeUnit.SECONDS)) {
-                    int exitValue = process.exitValue();
-                    websocketLog.sendBody("Program finished with exitcode " + exitValue);
+            Integer exitValue = awaitProgramFinish(process);
+            awaitOutput(executor);
 
-                    // TODO handle exitcode
-                } else {
-                    // timeout
-                    websocketLog.sendBody("Program timeout!");
-                }
+            if (exitValue != null) {
+                LOG.info("Program finished with exitcode " + exitValue);
+                websocketLog.sendBody("Program finished with exitcode " + exitValue);
+            } else {
+                LOG.info("Program finished abnormally");
+                websocketLog.sendBody("Program finished abnormally");
             }
-            finally {
-                process.destroy();
-            }
-        } catch (IOException|InterruptedException e) {
+        } catch (IOException e) {
             LOG.error("Program execution failure", e);
         }
     }
 
-    private void captureOutput(final InputStream inputStream, final OutType type) {
-        Thread outputThread = new Thread(() -> {
+    private Thread createOutputThread(final InputStream inputStream, final OutType type) {
+        return new Thread(() -> {
             try (BufferedReader brOut = new BufferedReader(new InputStreamReader(inputStream))) {
                 String line;
                 while ((line = brOut.readLine()) != null) {
@@ -81,7 +82,29 @@ public class AvrdudeRunner {
                 LOG.error("Error capturing " + type.name(), e);
             }
         });
-        outputThread.start();
     }
 
+    private void awaitOutput(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(MAX_TIME_FOR_PROGRAM_TO_FINISH, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            LOG.warn("Output prematurely interrupted", e);
+        }
+    }
+
+    private Integer awaitProgramFinish(Process process) {
+        Integer exitValue = null;
+        try {
+            if (process.waitFor(MAX_TIME_FOR_PROGRAM_TO_FINISH, TimeUnit.SECONDS)) {
+                exitValue = process.exitValue();
+            } else {
+                LOG.error("Waiting time elapsed before the subprocess has exited");
+            }
+        }
+        catch(InterruptedException e) {
+            LOG.error("Program thread interrupted while waiting for subprocess to finish", e);
+        }
+        return exitValue;
+    }
 }
