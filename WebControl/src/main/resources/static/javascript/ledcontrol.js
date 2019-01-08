3ea58a07f62cafd449c5b4627a234fa0220c2f1c
Stephan <stephanmahieu@yahoo.com>
2019-01-08 21:10:22 +0100

    Add websocket connect button, use pre-tag for monitor output, always scroll to bottom

--- WebControl/src/main/resources/static/javascript/ledcontrol.js
+++ WebControl/src/main/resources/static/javascript/ledcontrol.js
@@ -1,9 +1,13 @@
 let websocket;
+let wsConnectAttempt = 0;
+
+function getWebSocketUri() {
+    return "ws://" + (document.location.hostname === "" ? "localhost" : document.location.hostname) + ":9292/websocket-pub";
+}
 
 $(document).ready(function(){
     let dimObj = $("#dim");
     let dimSwitchObj = $("#manual-dim");
-    let statusObj = $('#input-status');
 
     dimObj.on("slidestop vclick blur", dimChangeHandler);
 
@@ -35,95 +39,17 @@ $(document).ready(function(){
         $('#input').text("");
     });
 
+    $("#connect").click(function(event) {
+        if (!$('#connect').hasClass('ui-state-disabled')) {
+            connectWebSocket();
+        }
+    });
+
     $('#uploadform').on('submit', uploadHandler);
 
     // Open a WebSocket connection.
-    let wsUri = getWebSocketRootUri() + "/websocket-pub";
-    websocket = new WebSocket(wsUri);
+    connectWebSocket();
 
-    // Connected to server
-    websocket.onopen = function(ev) {
-        statusObj.text('Connected to server');
-    };
-
-    // Connection close
-    websocket.onclose = function(ev) {
-        statusObj.text('Disconnected from server');
-    };
-
-    // Error
-    websocket.onerror = function(ev) {
-        statusObj.text(ev.data);
-    };
-
-    // Message Received
-    websocket.onmessage = function(ev) {
-        if (ev.data === "keep-alive") {
-            //websocket.send("echo-alive");
-            return;
-        }
-
-        let msgClass = "";
-        if (ev.data.match("^INT:")) {
-            msgClass = "debug";
-        } else if (ev.data.match("^INJ:")) {
-            msgClass = "received";
-        } else if (ev.data.match("^OUT:")) {
-            msgClass = "sent";
-        }
-
-        let msg = $('<div>').addClass("inputdata " + msgClass).text(ev.data);
-        $('#input').append(msg);
-
-        // use received status (json) to update the controls to reflect the current state
-        let indexAcc = ev.data.indexOf('{');
-        if (indexAcc >= 0) {
-            let jsonData = ev.data.substring(indexAcc);
-            let json = $.parseJSON(jsonData);
-            if ('STATUS' === json.type && json.status) {
-                let status = json.status;
-                if (status.effect) {
-                    let effectNo = effectToInt(json.status.effect);
-                    $(".current-effect").removeClass("current-effect");
-                    $("#" + effectNo).addClass("current-effect");
-                }
-                if (status.brightness) {
-                    if (dimObj.val() !== status.brightness) {
-                        // prevent triggering the changehandler and send a dim command!
-                        dimObj
-                            .off('slidestop vclick blur')
-                            .val(status.brightness).slider("refresh")
-                            .on('slidestop vclick blur', dimChangeHandler);
-                    }
-                }
-                if (status.autoBrightness === false) {
-                    // manual == on
-                    if (!dimSwitchObj.is(':checked')) {
-                        // prevent triggering the changehandler and send a command!
-                        dimSwitchObj
-                            .off("change")
-                            .prop('checked',true).flipswitch('refresh')
-                            .on("change", dimSwitchChangeHandler);
-                    }
-                    dimObj.slider('enable');
-                } else {
-                    // automatic == off
-                    if (dimSwitchObj.is(':checked')) {
-                        // prevent triggering the changehandler and send a command!
-                        dimSwitchObj
-                            .off("change")
-                            .prop('checked',false).flipswitch('refresh')
-                            .on("change", dimSwitchChangeHandler);
-                    }
-                    dimObj.slider('disable');
-                }
-            }
-        }
-
-        if (!isTouchDevice() && isStickingToBottom()) {
-            scrollToBottom();
-        }
-    };
 });
 
 function dimSwitchChangeHandler() {
@@ -150,34 +76,207 @@ function effectChangeHandler(event) {
     event.currentTarget.classList.add("current-effect");
 }
 
-function getWebSocketRootUri() {
-    return "ws://" + (document.location.hostname === "" ? "localhost" : document.location.hostname) + ":9292";
-}
+function connectWebSocket() {
+    // if (websocket && websocket.readyState === WebSocket.OPEN) {
+    //     // close so we can reconnect
+    //     websocket.close();
+    // }
 
-function isTouchDevice() {
+    // Open a WebSocket connection.
     try {
-        document.createEvent("TouchEvent");
-        return true;
-    } catch(e) {
-        return false;
+        websocket = new WebSocket(getWebSocketUri());
+
+        // Connected to server
+        websocket.onopen = function(ev) {
+            $('#input-status').text('Connected to server');
+            $('#connect').addClass('ui-state-disabled');
+            // resetWsTimeout();
+        };
+
+        // Connection close
+        websocket.onclose = function(ev) {
+            $('#input-status').text('Disconnected from server');
+            $('#connect').removeClass('ui-state-disabled');
+            websocket = null;
+            // resetWsTimeout();
+            // verifyAutoReconnectWebsocket();
+        };
+
+        // Error
+        websocket.onerror = function(ev) {
+            $('#input-status').text(ev.data);
+        };
+
+        websocket.onmessage = websocketMessageHandler;
+    }
+    catch(e) {
+        // ignore
     }
 }
 
-function scrollToBottom() {
-    let inputObj = $("#input");
-    let scrollHeight = inputObj.prop("scrollHeight");
-    //$('#input').scrollTop(scrollHeight);
-    inputObj.animate({scrollTop: scrollHeight}, 150);
+function websocketMessageHandler(ev) {
+    // Message Received
+    if (ev.data === "keep-alive") {
+        //websocket.send("echo-alive");
+        return;
+    }
+
+    let msgClass = "";
+    let msgData = ev.data;
+    if (ev.data.match("^INT:")) {
+        msgClass = "debug";
+    } else if (ev.data.match("^INJ:")) {
+        msgClass = "received";
+    } else if (ev.data.match("^OUT:")) {
+        msgClass = "sent";
+    } else if (ev.data.match("^STDOUT:")) {
+        msgClass = "stdout";
+        msgData = msgData.replace('STDOUT: ', '');
+    } else if (ev.data.match("^STDERR:")) {
+        msgClass = "stderr";
+        msgData = msgData.replace('STDERR: ', '');
+    }
+
+    let msg = $('<pre>').addClass("inputdata " + msgClass).text(msgData);
+    $('#input').append(msg);
+
+    if (isStatusUpdateResponse(ev.data)) {
+        initControlsWithStatusInfo(ev.data);
+    }
+
+    //if (!isTouchDevice() && isStickingToBottom()) {
+        scrollToBottom();
+    //}
 }
 
-function isStickingToBottom() {
-    let inputObj =  $("#input");
-    let scrollHeight = inputObj.prop("scrollHeight");
-    let innerHeight = inputObj.innerHeight();
-    let scrollMaxPos = scrollHeight - innerHeight;
-    let scrollPos = inputObj.scrollTop();
-    return (scrollMaxPos - scrollPos) < 25;
+function isStatusUpdateResponse(data) {
+    let indexAcc = data.indexOf('{');
+    if (indexAcc >= 0) {
+        let jsonData = data.substring(indexAcc);
+        let json = $.parseJSON(jsonData);
+        if ('STATUS' === json.type && json.status) {
+            return true;
+        }
+    }
+    return false
 }
+
+function initControlsWithStatusInfo(statusData) {
+    // use received status (json) to update the controls to reflect the current state
+    let dimObj = $("#dim");
+    let dimSwitchObj = $("#manual-dim");
+
+    let indexAcc = statusData.indexOf('{');
+    let jsonData = statusData.substring(indexAcc);
+    let json = $.parseJSON(jsonData);
+    let status = json.status;
+
+    if (status.effect) {
+        let effectNo = effectToInt(json.status.effect);
+        $(".current-effect").removeClass("current-effect");
+        $("#" + effectNo).addClass("current-effect");
+    }
+    if (status.brightness) {
+        if (dimObj.val() !== status.brightness) {
+            // prevent triggering the changehandler and send a dim command!
+            dimObj
+                .off('slidestop vclick blur')
+                .val(status.brightness).slider("refresh")
+                .on('slidestop vclick blur', dimChangeHandler);
+        }
+    }
+    if (status.autoBrightness === false) {
+        // manual == on
+        if (!dimSwitchObj.is(':checked')) {
+            // prevent triggering the changehandler and send a command!
+            dimSwitchObj
+                .off("change")
+                .prop('checked',true).flipswitch('refresh')
+                .on("change", dimSwitchChangeHandler);
+        }
+        dimObj.slider('enable');
+    } else {
+        // automatic == off
+        if (dimSwitchObj.is(':checked')) {
+            // prevent triggering the changehandler and send a command!
+            dimSwitchObj
+                .off("change")
+                .prop('checked',false).flipswitch('refresh')
+                .on("change", dimSwitchChangeHandler);
+        }
+        dimObj.slider('disable');
+    }
+}
+
+// function autoReconnectWebsocket() {
+//     if (websocket) {
+//         websocket.close();
+//
+//     }
+//     connectWebSocket();
+//     setTimeout(verifyAutoReconnectWebsocket, getWsTimeout());
+// }
+//
+// function verifyAutoReconnectWebsocket() {
+//     if (websocket) {
+//         switch (websocket.readyState) {
+//             case WebSocket.OPEN:
+//                  return;
+//
+//             case WebSocket.CONNECTING:
+//                  setTimeout(verifyAutoReconnectWebsocket, 1000);
+//                  break;
+//
+//             case WebSocket.CLOSED:
+//             case WebSocket.CLOSING:
+//                 setTimeout(autoReconnectWebsocket, getWsTimeout());
+//                 break;
+//         }
+//     } else {
+//         setTimeout(autoReconnectWebsocket, getWsTimeout());
+//     }
+// }
+//
+// function getWsTimeout() {
+//     wsConnectAttempt++;
+//     if (wsConnectAttempt > 50) {
+//         return 30 * 60 * 1000; // 30 min
+//     }
+//     else if (wsConnectAttempt > 20) {
+//         return 5 * 60 * 1000; // 5 min
+//     }
+//     else if (wsConnectAttempt > 10) {
+//         return 60 * 1000; // 1 min
+//     }
+//     return 5000; // 5 sec
+// }
+//
+// function resetWsTimeout() {
+//     wsConnectAttempt = 0;
+// }
+
+function scrollToBottom() {
+    let inputObj = $("#input");
+    inputObj.stop().animate({"scrollTop": inputObj.prop("scrollHeight")}, 300);
+}
+
+// function isTouchDevice() {
+//     try {
+//         document.createEvent("TouchEvent");
+//         return true;
+//     } catch(e) {
+//         return false;
+//     }
+// }
+
+// function isStickingToBottom() {
+//     let inputObj =  $("#input");
+//     let scrollHeight = inputObj.prop("scrollHeight");
+//     let innerHeight = inputObj.innerHeight();
+//     let scrollMaxPos = scrollHeight - innerHeight;
+//     let scrollPos = inputObj.scrollTop();
+//     return (scrollMaxPos - scrollPos) < 25;
+// }
 
 function effectToInt(name) {
     switch (name) {
