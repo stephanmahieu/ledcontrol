d95d21591d3a0cee3f3e4db5c1a419047607260b
Stephan <stephanmahieu@yahoo.com>
2019-01-14 01:45:09 +0100

    Enable remote debugging

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @echo off
 rem set properties.dir=classpath:
-java -jar WebControl\target\WebControl.jar
+rem java -jar WebControl\target\WebControl.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar WebControl\target\WebControl.jar
